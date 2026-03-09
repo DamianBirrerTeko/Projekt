80feeb586c0c033e7c54d3da4db6091ea1bfa244
DamianBirrerTeko <damian.birrer@edu.teko.ch>
2026-03-09 17:08:45 +0100

    Implemented find_best_time function, changed url in get_ha_data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import pandas as pd
+from datetime import timedelta
 from dotenv import load_dotenv
 
 # Lade Umgebungsvariablen aus einer .env Datei, aus Sicherheitsgründen.
@@ -11,7 +12,7 @@ SENSOR_NEXT_HOUR = os.getenv("SENSOR_NEXT_HOUR")
 
 def get_ha_data(SENSOR_NEXT_HOUR):
     # Holt die Prognosedaten von der Home Assistant API.
-    url = f"{HA_URL}/api/states/{SENSOR_NEXT_HOUR}"
+    url = f"{HA_URL}{SENSOR_NEXT_HOUR}"
     headers = {
         "Authorization": f"Bearer {HA_TOKEN}",
         "content-type": "application/json",
@@ -25,6 +26,32 @@ def get_ha_data(SENSOR_NEXT_HOUR):
         print(f"Fehler bei der API-Abfrage: {e}")
         return None
 
+def find_best_time(forecast_json, duration_hours=2):
+    # Berechnet das Zeitfenster mit dem höchsten Ertrag.
+    # Extrahiere die stündlichen Daten aus den Attributen
+    forecast_list = forecast_json.get('attributes', {}).get('forecast', [])
+    
+    if not forecast_list:
+        print("Keine Forecast-Daten gefunden.")
+        return None
+
+    # Daten in ein Pandas DataFrame umwandeln
+    df = pd.DataFrame(forecast_list)
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df.set_index('datetime', inplace=True)
+
+    # Sliding Window: Summiere die Werte über Anzahl Stunden
+    # 'rolling' schaut sich immer die Fenster der angegebenen Stunden an
+    df['rolling_sum'] = df['production'].rolling(window=duration_hours).sum()
+
+    # Finde den Zeitpunkt mit der maximalen Summe
+    best_slot_end = df['rolling_sum'].idxmax()
+    
+    # Da rolling(2) am Ende des Fensters markiert, ziehen wir die Dauer ab
+    best_start_time = best_slot_end - timedelta(hours=duration_hours - 1)
+    
+    return best_start_time
+
 def analyze_solar_peak(data, duration=2):
     """
     Simpler Algorithmus zur Peak-Suche.
@@ -34,6 +61,8 @@ def analyze_solar_peak(data, duration=2):
     print(f"Analysiere Daten für ein Zeitfenster von {duration} Stunden...")
     pass
 
+
+
 if __name__ == "__main__":
     if not HA_TOKEN:
         print("Fehler: Kein HA_TOKEN in der .env gefunden!")
@@ -42,3 +71,4 @@ if __name__ == "__main__":
         if forecast_data:
             print(f"Daten erfolgreich empfangen: {forecast_data.get('state')} kWh")
             analyze_solar_peak(forecast_data)
+            print(f"Beste Startzeit: {find_best_time(forecast_data)}")
