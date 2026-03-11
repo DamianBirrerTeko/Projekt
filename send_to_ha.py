8a0101f9738f8dd0d1d9b2861856d7148ba5f820
DamianBirrerTeko <damian.birrer@edu.teko.ch>
2026-03-11 21:28:17 +0100

    created funtions to send result to ha

---
+++ send_to_ha.py
@@ -0,0 +1,29 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+HA_URL = os.getenv("HA_URL")
+HA_TOKEN = os.getenv("HA_TOKEN")
+
+def send_to_homeassistant(start_time):
+    # Übermittelt den Zeitpunkt an den Home Assistant Helfer.
+    url = f"{HA_URL}/api/services/input_datetime/set_datetime"
+    headers = {
+        "Authorization": f"Bearer {HA_TOKEN}",
+        "content-type": "application/json",
+    }
+    
+    payload = {
+        "entity_id": "input_datetime.solar_optimierter_start",
+        "datetime": start_time.strftime('%Y-%m-%d %H:%M:%S')
+    }
+    
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        if response.status_code == 200:
+            print(f"\nErfolg: Startzeit wurde an Home Assistant gesendet.\n")
+        else:
+            print(f"\nFehler beim Senden: {response.status_code} - {response.text}\n")
+    except Exception as e:
+        print(f"\nVerbindungsfehler zu Home Assistant: {e}\n")
